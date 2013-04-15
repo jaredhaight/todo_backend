c0de99def25276a40a873daa2a6ef9c7c23045c4
Jared <jared@photosandtext.com>
2013-04-15 14:52:08 -0400

    Cleaned up angular code a bit.

--- todojs/controllers.js
+++ todojs/controllers.js
@@ -4,6 +4,5 @@
 
 function todoListCtrl($scope, todoListClient, $routeParams) {
     var data = todoListClient.get();
-    $scope.todoList = todoListClient;
     $scope.todoListData = data;
 }
--- todojs/services.js
+++ todojs/services.js
@@ -4,7 +4,7 @@
 
 angular.module('todoListSvc', ['ngResource']).
     factory('todoListClient', function($resource){
-        return $resource('http://127.0.0.1:port/api/todo/.jsonp?callback=JSON_CALLBACK', {port:':8000'}, {
+        return $resource('http://127.0.0.1:port/api/todo/?format=jsonp&callback=JSON_CALLBACK', {port:':8000'}, {
             get: {method:'JSONP', isArray:false}
         });
     });
