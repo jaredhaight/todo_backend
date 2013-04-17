5522e2a802a7364e7cc5146f16f0be7b568e1bd2
Jared <jared@photosandtext.com>
2013-04-17 10:47:01 -0400

    Added token header to request to authenticate against REST API. Will make this more secure later (as opposed to hardcoding the value).

--- todojs/controllers.js
+++ todojs/controllers.js
@@ -2,7 +2,15 @@
 
 /* Controllers */
 
-function todoListCtrl($scope, todoListClient, $routeParams) {
-    var data = todoListClient.get();
-    $scope.todoListData = data;
+function todoListCtrl($scope, todoListClient, $routeParams, $http) {
+    $http.defaults.headers.post['Authorization'] = 'Token 09c34730916dee2ccaa71b6c35d2eba689e554b4';
+    $scope.reverseAge = 'true';
+    $scope.todoListData = todoListClient.get();
+    $scope.addTodo = function(){
+        $scope.todoListData.name = $scope.todoName;
+        $scope.todoListData.desc = $scope.todoDesc;
+        $scope.todoListData.$save(function(){
+            $scope.todoListData = todoListClient.get();
+        });
+    };
 }
