fdd7778a94e853faec7c7fd3b139f54c8c36bc5b
Jared <jared@photosandtext.com>
2013-04-17 10:46:03 -0400

    Oh, so that's how resource works in angular. I was over complicating things.

--- todojs/services.js
+++ todojs/services.js
@@ -4,7 +4,5 @@
 
 angular.module('todoListSvc', ['ngResource']).
     factory('todoListClient', function($resource){
-        return $resource('http://127.0.0.1:port/api/todo/?format=jsonp&callback=JSON_CALLBACK', {port:':8000'}, {
-            get: {method:'JSONP', isArray:false}
+        return $resource('http://127.0.0.1:port/api/todo/?:arg1:arg2', {port:':8000'}, {});
         });
-    });
