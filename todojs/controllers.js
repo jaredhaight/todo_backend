c26f0bc1fe7fd2973d5bb499e0ec2733fb2771b0
Jared <jared@photosandtext.com>
2013-04-21 13:49:01 -0400

    Created complete and edit functions. Functions edit view directly now instead of doing a GET to refresh list.

--- todojs/controllers.js
+++ todojs/controllers.js
@@ -5,27 +5,25 @@
 function todoListCtrl($scope, todoListClient, $routeParams, $http) {
     $http.defaults.headers.common['Authorization'] = 'Token 09c34730916dee2ccaa71b6c35d2eba689e554b4';
     $scope.reverseAge = 'true';
+    $scope.editing = 'false';
     var todoList = todoListClient.get();
     $scope.todos = todoList;
     $scope.addTodo = function(){
-        console.log('addTodo')
-        $scope.todos.name = $scope.todoName;
-        $scope.todos.desc = $scope.todoDesc;
-        $scope.todos.$save(function(){
-            $scope.todos = todoListClient.get();
+        var newTodo = new todoListClient;
+        newTodo.name = $scope.todoName;
+        newTodo.desc = $scope.todoDesc;
+        newTodo.$save(function(){
+            $scope.todos.results.push(newTodo);
         });
     };
     $scope.deleteTodo = function (todo, index) {
         console.log('deleteTodo');
         console.log(index);
         $scope.todos.$delete({todoID: todo.id}, function() {
-            $scope.todos.results.splice(index, 1);
+            $scope.todos.results.splice($scope.todos.results.indexOf(todo), 1);
         });
     };
-    $scope.getTodo = function(todo){
-        console.log("getTodo");
-        $scope.json = todoListClient.get({todoID: todo.id})
-        };
+
     $scope.todoCompleted = function(todo, index){
         console.log("todoCompleted");
         var todoobj = todoListClient.get({todoID: todo.id});
@@ -36,8 +34,38 @@ function todoListCtrl($scope, todoListClient, $routeParams, $http) {
         console.log(todoobj.completed);
         console.log(index);
         todoobj.$update({todoID: todo.id}, function(){
+            var index = $scope.todos.results.indexOf(todo);
             $scope.todos.results[index].completed = now;
+            $scope.todos.results[index].updated = now;
             console.log('Todo saved as completed');
+        })
+    };
+
+    $scope.enableEdit = function(todo) {
+        $scope.todoName = todo.name;
+        $scope.todoDesc = todo.desc;
+        $scope.editing = 'true';
+        $scope.todoEditObj = todo;
+        console.log($scope.editing);
+    };
+
+    $scope.undoEdit = function() {
+        $scope.todoName = '';
+        $scope.todoDesc = '';
+        $scope.todoEditObj = null;
+        $scope.editing = 'false';
+    };
+
+    $scope.editTodo = function(todo) {
+        var todoObj = todoListClient.get({todoID: todo.id});
+        todoObj.name = $scope.todoName;
+        todoObj.desc = $scope.todoDesc;
+        todoObj.$update({todoID: todo.id}, function(){
+            var index = $scope.todos.results.indexOf(todo);
+            $scope.todos.results[index].name = $scope.todoName;
+            $scope.todos.results[index].desc = $scope.todoDesc;
+            $scope.todos.results[index].updated = new Date();
+            $scope.undoEdit();
         })
     }
 }
