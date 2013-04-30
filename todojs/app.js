30f92b66ab7a14f7f018318da52f0f01af1d6d6e
Jared <jared@photosandtext.com>
2013-04-29 22:37:12 -0400

    Added user based routing and pages.

--- todojs/app.js
+++ todojs/app.js
@@ -4,6 +4,7 @@
 // Declare app level module which depends on filters, and services
 angular.module('todojs', ['todojs.filters', 'todojs.directives','todoListSvc']).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/list', {templateUrl: 'partials/list.html', controller: todoListCtrl});
+    $routeProvider.when('/list', {templateUrl: 'partials/todolist.html', controller: todoListCtrl});
+    $routeProvider.when('/user/:username', {templateUrl: 'partials/userlist.html', controller: userListCtrl});
     $routeProvider.otherwise({redirectTo: '/list'});
   }]);
